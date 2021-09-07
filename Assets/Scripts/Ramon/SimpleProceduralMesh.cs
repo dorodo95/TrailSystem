14043c7aff8f2cbf30b420999c99a8b89b7d5161
Ramon Camata <ramoncamata@gmail.com>
2021-09-07 02:10:11 -0300

    first pass procedural Mesh test

---
+++ Assets/Scripts/Ramon/SimpleProceduralMesh.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SimpleProceduralMesh : MonoBehaviour
+{
+
+    //public Vector3[] positions;
+    List<Vector3> finalPos;
+    Mesh mesh;
+    public Transform[] positions;
+    public int[] faces;
+
+    void Awake ()
+    {
+        mesh = new Mesh();
+        mesh.name = "Procedural Mesh";
+        SetVertices ();
+    }
+    void SetVertices ()
+    {
+        finalPos = new List<Vector3>();
+        for(var i = 0; i < positions.Length; i++){
+            finalPos.Add(positions[i].position);
+        }
+    }
+    void Update ()
+    {
+
+        if(finalPos.Count != positions.Length)
+        {
+          SetVertices ();
+        }
+        
+ 
+        for(var i = 0; i < positions.Length; i++){
+           
+           finalPos[i] = positions[i].position;
+           
+        }
+
+        mesh.SetVertices ( finalPos );
+        mesh.triangles = faces;
+    
+    GetComponent<MeshFilter>().sharedMesh = mesh;
+
+    }
+
+
+}
